7337104a626552b73fd896d9637e29a6f7ab1c55
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-03-17 14:01:09 +0300

    Time: 2 ms (34.86%), Space: 5.8 MB (17.71%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.c
@@ -0,0 +1,11 @@
+char * toLowerCase(char * s){
+    int i = 0;
+    
+    while(s[i])
+    {
+        if(s[i] >= 65 && s[i] <= 90)
+            s[i] = s[i] + 32;
+        i++;
+    }
+    return s;
+}
