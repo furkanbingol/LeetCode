777a591ce6f77512a7d0d8cafcbc932c6ef8d587
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-03-18 23:56:50 +0300

    Time: 0 ms (100.00%), Space: 5.7 MB (19.06%) - LeetHub

---
+++ 258-add-digits/258-add-digits.c
@@ -0,0 +1,20 @@
+int addDigits(int num){
+    int s = 0;
+    while (num > 0)
+    {
+        s += num % 10;
+        num /= 10;
+    }
+    while (s >= 10)
+    {
+        int tmp = s;
+        s = 0;
+        while (tmp > 0)
+        {
+            s += tmp % 10;
+            tmp /= 10;
+        }
+    }
+    
+    return s;
+}
