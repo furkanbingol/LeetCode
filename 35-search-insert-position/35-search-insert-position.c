cb53e9d97dc5041ec9e180121b57c0ea8f43854f
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-04-05 10:39:29 +0300

    Update 35-search-insert-position.c

--- 35-search-insert-position/35-search-insert-position.c
+++ 35-search-insert-position/35-search-insert-position.c
@@ -1,14 +1,21 @@
 int searchInsert(int* nums, int numsSize, int target){
-    int i;
+    int l,r,mid;
+    l = 0;
+    r = numsSize-1;
     
-    for(i = 0; i < numsSize; ++i)
+    while(l <= r)
     {
-        if(nums[i] == target)
-            return i;
-        else if(i != numsSize-1 && (nums[i] < target && target < nums[i+1]))
-            return i+1;
-        else if(target < nums[0])
-            return 0;
+        mid = (l+r)/2;
+        if(target > nums[numsSize-1])
+            return numsSize;
+        else if(nums[mid] == target)
+            return mid;
+        else if(target > nums[mid] && target < nums[mid+1])
+            return mid+1;
+        else if(target < nums[mid])
+            r = mid-1;
+        else if(target > nums[mid])
+            l = mid+1;
     }
-    return numsSize;
+    return 0;
 }
