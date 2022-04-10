db9399428c41fd03943702c748f100520bb37c0f
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-04-10 12:38:24 +0300

    Time: 0 ms (100.00%), Space: 5.4 MB (96.01%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.c
+++ 371-sum-of-two-integers/371-sum-of-two-integers.c
@@ -1,19 +1,11 @@
 int getSum(int a, int b){
-    int ans = 0;
-    unsigned char abit, bbit, i, carry;
-    long sum;
-    
-    for (i = 0, carry = 0; i < 32; ++i) {
-        abit = a & 1;
-        bbit = b & 1;
-        sum = carry ^ abit ^ bbit;
-        
-        ans |= (sum << i);
-        carry = abit & bbit | (carry & (abit ^ bbit));
-        
-        a >>= 1;
-        b >>= 1;
+    unsigned int sum = (unsigned int)a ^ (unsigned int)b;
+    unsigned int carry = ((unsigned int)a & (unsigned int)b) << 1;
+    while(carry != 0)
+    {
+        unsigned int temp = sum ^ carry;
+        carry = (sum & carry) << 1;
+        sum = temp;
     }
-    
-    return ans;
+    return (int)sum;
 }
