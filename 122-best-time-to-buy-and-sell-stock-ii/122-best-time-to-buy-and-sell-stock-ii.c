8750aa25a93b4f987528fbbb4b6fb9286ca5f372
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-04-16 12:17:03 +0300

    Time: 6 ms (63.69%), Space: 6.6 MB (87.35%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.c
@@ -0,0 +1,8 @@
+int maxProfit(int* prices, int pricesSize) {
+    int sum = 0;
+    for (int i = 1; i < pricesSize; ++i) {
+        if (prices[i - 1] < prices[i])
+            sum += (prices[i] - prices[i - 1]);
+    }
+    return sum;
+}
