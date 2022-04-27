6c5b79a9f463d02c9559f6f0c38f596bd9b76fa7
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-04-27 15:16:45 +0300

    Time: 6 ms (8.35%), Space: 5.6 MB (39.75%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,12 @@
+int numberOfSteps(int num){
+    int c = 0;
+    while(num > 0)
+    {
+        if(num % 2 == 0)
+            num /= 2;
+        else
+            num -= 1;
+        ++c;
+    }
+    return c;
+}
