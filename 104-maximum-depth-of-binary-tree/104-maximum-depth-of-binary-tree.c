6e9a270b57a36a18863a45690995ac3838b092b2
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-05-15 22:12:29 +0300

    Update 104-maximum-depth-of-binary-tree.c

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.c
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.c
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-
 int maxDepth(struct TreeNode* root){
     if(root == NULL)
         return 0;
