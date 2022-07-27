a1f3c46ea3c8ffe7dd883393b7e09c4c072409d7
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-07-27 19:54:46 +0300

    Time: 19 ms (52.11%), Space: 7 MB (75.51%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.c
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.c
@@ -1,15 +1,12 @@
 int firstUniqChar(char * s){
     int count[26] = {0};
-    int index[26] = {-1};
         
-    for(int i = 0; s[i]; i++){
+    for(int i = 0; s[i]; i++)
         count[s[i] - 'a']++;
-        index[s[i] - 'a'] = i;
-    }
     
     for(int i = 0; s[i]; i++){
         if(count[s[i] - 'a'] == 1)
-            return index[s[i] - 'a'];
+            return i;
     }
     return -1;
 }
