fffe54d7f712ac05f9bf6ffda68b8e717f488fcf
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-10 23:21:59 +0300

    Time: 10 ms (73.98%), Space: 16.4 MB (27.31%) - LeetHub

---
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+struct TreeNode* bst(int* nums, int start, int end){
+    if(start > end)
+        return NULL;
+    int mid = (start+end)/2;
+    
+    struct TreeNode* root = malloc(sizeof(struct TreeNode));
+    
+    root->val = nums[mid];
+    root->left = bst(nums, start, mid-1);
+    root->right = bst(nums, mid+1, end);
+    
+    return root;
+}
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize){
+    int start = 0, end = numsSize-1;
+    int mid = (start+end)/2;
+    
+    return bst(nums, start, end);
+    
+}
