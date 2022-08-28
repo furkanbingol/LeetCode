ddb681e7733ad58a514d9b9228ec7c74605f3aa1
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-28 19:43:30 +0300

    Time: 1 ms (99.92%), Space: 48.8 MB (99.43%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length-1;
+        char tmp;
+        
+        while(l < r){
+            tmp = s[l];
+            s[l] = s[r];
+            s[r] = tmp;
+            l++;
+            r--;
+        }
+    }
+}
