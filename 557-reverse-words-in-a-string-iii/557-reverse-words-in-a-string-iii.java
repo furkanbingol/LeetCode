794d3ede89f6731b766209f57ed3310e59679326
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-08-29 22:56:17 +0300

    Time: 11 ms (57.15%), Space: 53.4 MB (34.22%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++)
+            str[i] = new StringBuilder(str[i]).reverse().toString();
+
+        StringBuilder result = new StringBuilder();
+        for (String st : str)
+            result.append(st + " ");
+        return result.toString().trim();  //trim() is used for delete the last ' '(space)
+    }
+}
