4d48d2ed032f343441a4bdfe9b2b573c9db93ed6
Furkan <94539804+furkanbingol@users.noreply.github.com>
2022-09-04 23:36:17 +0300

    Time: 1 ms (65.55%), Space: 42 MB (47.52%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length())
+            return false;
+        String str = s + s;
+        return (str.contains(goal));
+    }
+}
