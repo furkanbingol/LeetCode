6333c9739e2f519b40c5e296411d9de98ea04fc3
Furkan <94539804+furkanbingol@users.noreply.github.com>
2023-05-24 13:32:02 +0300

    Time: 14 ms (41.67%), Space: 13.9 MB (87.50%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func findMiddleIndex(_ nums: [Int]) -> Int {
+        var rSum = 0
+        var lSum = 0
+        
+        for i in nums { 
+            rSum += i
+        }
+        
+        for (i,val) in nums.enumerated() {
+            rSum -= val
+            if lSum == rSum {
+                return i
+            }
+            lSum += val
+        }
+        return -1
+    }
+}
