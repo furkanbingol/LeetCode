2f5409480dffc5abb834a81711d3a89bf0d0029a
Furkan <94539804+furkanbingol@users.noreply.github.com>
2023-09-04 12:05:16 +0300

    Time: 71 ms (85.71%), Space: 16.2 MB (71.43%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+class Solution {
+    var arr = [Int]()
+
+    init(_ head: ListNode?) {
+        var tmp = head
+        while tmp != nil {
+            arr.append(tmp!.val)
+            tmp = tmp!.next
+        }
+    }
+    
+    func getRandom() -> Int {
+        return arr.randomElement()!
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * let obj = Solution(head)
+ * let ret_1: Int = obj.getRandom()
+ */
